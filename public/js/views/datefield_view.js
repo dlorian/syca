498a7d4172b33c427d1cd2b0272616022f2df261
Florian Dorau <f.dorau@ymail.com>
2014-02-25 20:37:09 +0100

    Dateview updated and refactored

--- public/js/views/datefield_view.js
+++ public/js/views/datefield_view.js
@@ -1,11 +1,15 @@
 EmberApp.DateField = Ember.View.extend({
     // Layout for the view.
-    layout: Ember.Handlebars.compile('<input type="text" class="validate form-control" id="datepicker">'),
-    attributeBindings: ['date', 'disabled', 'name'],
-    date: null,         // Stores the date object, which was set either manually by the input field or the datepicker.
-    value: null,        // Stores the string representation of the date object, which is stored in the date property.
-    disabled: false,    // Stroes the state of the input field.
+    layout: Ember.Handlebars.compile(
+        '<input type="text" class="validate form-control" id="datepicker">'
+    ),
+    attributeBindings: ['date', 'disabled', 'name', 'placeholder', 'helpTextEnabled'],
+    date: null,         // Stores the ISO-String for the raw date. Also used as the return value of the field.
+    rawDate: null,      // Stores the raw JavaScript Date object
+    disabled: false,    // Stores the state of the input field.
     name: null,
+    placeholder: null,
+    helpTextEnabled: true,
 
     // Used jQuery elements
     $datepicker: null,  // jQuery element of the datepicker.
@@ -32,84 +36,112 @@ EmberApp.DateField = Ember.View.extend({
         this.$datepicker = this.$('#datepicker');
 
         // Setup of the datepicker
-        this.$datepicker.datepicker({ format: 'dd.mm.yyyy', weekStart: 1 })
+        this.$datepicker.datepicker({
+            format: 'dd.mm.yyyy',
+            weekStart: 1
+        })
         .on('changeDate', function(event) {
-            me.set('date', event.date);
+            me.set('rawDate', event.date);
         })
         .on('change', function() {
-            me.set('value', me.$datepicker.val());
+            me.set('rawDate', me.$datepicker.val());
         });
 
         // Set input field disabled if necessary
         this.updateDisabled();
         this.updateName();
+        this.updatPlaceholder();
 
         // Initialize the value of the datepicker
         var date = this.get('date');
         if(date) {
+            // Save the inital date as the raw date value.
+            // date will hold the corresponding ISO-String.
+            this.set('rawDate', date);
+
             // Do not set an undefined date to the datepicker
-            this.$datepicker.datepicker('setValue', date);
+            this.$datepicker.datepicker('setValue', rawDate);
         }
     },
 
     updateDisabled: function() {
-        var disabled = this.get('disabled');
-        this.$datepicker.attr('disabled', disabled);
+        this.$datepicker.attr('disabled', this.get('disabled'));
     }.observes('disabled'),
 
     updateName: function() {
-        var name = this.get('name');
-        this.$datepicker.attr('name', name);
+        this.$datepicker.attr('name', this.get('name'));
     }.observes('name'),
 
-    /**
-     * Updates the value porperty if the date property has changed.
-     * Creates a corresponding string represantion for the date
-     * object.
-     */
-    updateValue: function() {
-        var date = this.get('date');
-        var value = this.get('value');
+    updatPlaceholder: function() {
+        this.$datepicker.attr('placeholder', this.get('placeholder'));
+    }.observes('placeholder'),
 
-        var isValid = this.isValidDate(date);
-        if(isValid && value !== this.getValueForDate(date)) {
-            this.set('value', date);
+    /**
+     * Updates the date property regarding the new raw date.
+     */
+    updateDate: function() {
+        var rawDate = this.get('rawDate');
+
+        // Verify that the given date is valid
+        var isValid = this.isValidDate(rawDate);
+
+        if(isValid) {
+            var date = this.rawDate2Date(rawDate);
+            // Save the ISO-String for server side
+            this.set('date', date.toISOString());
         }
-        // mark input field valid or invalid, regarding isValid
+
+        // Mark the field as valid or invaid
         this.markAsInvalid(!isValid);
+    }.observes('rawDate'),
+
+    /**
+     * Updates the rawDate property regarding the new date. If the date
+     * is set from the outside we have to update the raw date value.
+     */
+    updateRawDate: function() {
+        var date = this.get('date');
+
+        if(date === null) {
+            // If date was set to null, clear datepicker value
+            this.$datepicker.val(null);
+            return;
+        }
+
+        // Check if date is a new date
+        var rawDate = this.date2RawDate(date);
+        if(rawDate && this.get('rawDate').getTime() !== rawDate.getTime()) {
+            this.set('rawDate', date);
+        }
     }.observes('date'),
 
     /**
-     * Updates the date property if the value property has changed.
-     * Creates a date object based on the string represantion of the
-     * value attribute.
+     * Converts the given date string into a JavaScript Date object.
+     * It is necessary that the date is a valid date object or a date
+     * string in the correct format.
      */
-    updateDate: function() {
-        var value = this.get('value');
-        var isValid = this.isValidDate(value);
-        if(isValid) {
-            var date = this.getDateForValue(value);
-            this.set('date', date);
+    rawDate2Date: function(rawDate) {
+        if(rawDate === null || rawDate === '') {
+            return '';
         }
-        //this.markAsInvalid(!isValid);
-    }.observes('value'),
-
-    /**
-     * Returns the expected string representation for the given date.
-     */
-    getValueForDate: function(date) {
-        return moment(date).format(this.get('validDateString'));
+        if(rawDate instanceof Date) {
+            return rawDate;
+        }
+        else if(typeof rawDate === 'string') {
+            return moment(rawDate, this.get('validDateString')).toDate();
+        }
     },
 
     /**
-     * Returns a date object for the given string representation of the date.
+     * Converts the given date string into a JavaScript Date object.
+     * Returns a date object if date is a valid date string, else null.
      */
-    getDateForValue: function(value) {
-        if(value instanceof Date) {
-            return value;
+    date2RawDate: function(date) {
+        try {
+           return moment(date).toDate();
         }
-        else if(typeof value === 'string') {
-            return moment(value, this.get('validDateString')).toDate();
+        catch(err) {
+            return null;
         }
     },
 
@@ -122,6 +154,10 @@ EmberApp.DateField = Ember.View.extend({
      */
     isValidDate: function(date) {
         var isValid = false;
+        if(date === null || date === '') {
+            // treat null as valid value, to make it possible to clear
+            isValid = true;
+        }
         if(date instanceof Date && moment(date).isValid()) {
             isValid = true;
         }
@@ -153,7 +189,10 @@ EmberApp.DateField = Ember.View.extend({
      * Shows the help text block.
      */
     showHelp: function() {
-        $(this.helpBlock).insertAfter('#datepicker').html(this.helpText);
+        debugger
+        if(this.get('helpTextEnabled')) {
+            $(this.helpBlock).insertAfter('#datepicker').html(this.helpText);
+        }
     },
 
     /**
