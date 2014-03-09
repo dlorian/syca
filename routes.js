6d335f68ee8e7e01b1b66e997d8ee033fbd03c22
Florian Dorau <f.dorau@ymail.com>
2014-03-09 11:52:05 +0100

    Ugrade express version from 3. to 4.

--- routes.js
+++ routes.js
@@ -1,83 +1,89 @@
 
-var User = require('./models/user');
-
-var tours = require('./lib/tour');
-var home = require('./lib/home');
+var User    = require('./models/user');
+var tours   = require('./lib/tour');
+var home    = require('./lib/home');
 var weather = require('./lib/weather');
 
 module.exports = function(app, passport) {
 
-    // get the logged in user
-    app.get('/api/login', function(req, res, next) {
-        console.log('GET#login');
+    // Set up Login route
+    app.route('/api/login')
 
-        console.log('Check if user is authenticated');
-        if(req.isAuthenticated()) {
-            console.log('User is authenticated: %s', req.user.username);
+        .get(function(req, res, next) {     // Get the logged in user
+            console.log('GET#login');
+            console.log('Check if user is authenticated');
 
-            setTimeout(function(){
-                return res.send({ success: true, user: req.user.username });
-                //return res.send("401 unauthorized", 500);
-            },1);
+            if(req.isAuthenticated()) {
+                console.log('User is authenticated: %s', req.user.username);
 
-        }
-        else {
-            console.log('User is NOT authenticated');
-            return res.send({ success: false, msg: 'Not logged in.' });
-        }
-    });
+                setTimeout(function(){
+                    return res.send({ success: true, user: req.user.username });
+                    //return res.send("401 unauthorized", 500);
+                }, 1);
+            }
+            else {
+                console.log('User is NOT authenticated');
+                return res.send({ success: false, msg: 'Not logged in.' });
+            }
+        })
 
-    // login routes
-    app.post('/api/login', function(req, res, next) {
-        console.log('POST#login');
-        // setTimeout for testing prupose to simulate traffic
-        setTimeout(function() {
-            passport.authenticate('local', function(err, user, info) {
-                if (err) {
-                    console.log('Error occured while log in user %s.', user.username);
-                    return next(err);
-                }
+        .post(function(req, res, next) {
+            console.log('POST#login');
 
-                if (!user) {
-                    console.log('Log in of user %s NOT successfull', user.username);
-                    return res.send({success: false, msg: 'Benutzername oder Passwort ist falsch.'});
-                }
-
-                // log in user
-                console.log('Try to log in user %s.', user.username);
-                req.logIn(user, function(err) {
+            setTimeout(function() {
+                passport.authenticate('local', function(err, user, info) {
                     if (err) {
-                        console.log('Error occured while loggin in user: %s.', user.username);
+                        console.log('Error occured while log in user %s.', user.username);
                         return next(err);
                     }
-                    console.log('Login of user %s was successfull.', user.username);
 
-                    return res.send({success: true, msg: 'Login erfolgreich.', user: user.username});
-                });
-            })(req, res, next); // add net for error handling
-        }, 1);
-    });
+                    if (!user) {
+                        console.log('Log in of user %s NOT successfull', user.username);
+                        return res.send({success: false, msg: 'Benutzername oder Passwort ist falsch.'});
+                    }
 
-    app.post('/api/logout', function(req, res) {
-      req.logout();
-      res.send({success: true, msg: 'Logout erfolgreich.'});
-    });
+                    // log in user
+                    console.log('Try to log in user %s.', user.username);
+                    req.logIn(user, function(err) {
+                        if (err) {
+                            console.log('Error occured while loggin in user: %s.', user.username);
+                            return next(err);
+                        }
+                        console.log('Login of user %s was successfull.', user.username);
+                        return res.send({success: true, msg: 'Login erfolgreich.', user: user.username});
+                    });
+                })(req, res, next); // add net for error handling
+            }, 1);
+        })
+    ;
 
-    // tour routes
-    app.get('/api/tours',     isLoggedIn, tours.find);
-    app.get('/api/tours/:id', isLoggedIn, tours.findById);
-    app.post('/api/tours',    isLoggedIn, tours.add);
-    app.put('/api/tours/:id', isLoggedIn, tours.update);
-    //app.delete('/api/tours/:id', tours.delete); not yet implemented
+    // Set up logout route
+    app.route('/api/logout')
+        .post(function(req, res) {
+            req.logout();
+            res.send({success: true, msg: 'Logout erfolgreich.'});
+        })
+    ;
+
+    // Set up tours routes
+    app
+        .get('/api/tours', isLoggedIn, tours.find)
+        .get('/api/tours:id', isLoggedIn, tours.findById)
+        .post('/api/tours', isLoggedIn, tours.add)
+        .put('/api/tours:id', isLoggedIn, tours.update)
+        //.delete('/api/tours/:id', tours.delete); not yet implemented
+    ;
 
     //app.get('/homes', home.find);
 
-    // weather routes
-    app.get('/api/weather', weather.getWeatherDataById);
-    app.get('/api/weather/find?:city', weather.getCityByText);
-}
+    // Set up weather routes
+    app.route('/api/weather')
+        .get(weather.getWeatherDataById)
+        .get('find?:city', weather.getCityByText)
+    ;
+};
 
-// route middleware to make sure a user is logged in
+//Middleware to verify that a user is logged in
 function isLoggedIn(req, res, next) {
 
     // if user is authenticated in the session, carry on
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var path = require ('path');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var express        = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var session        = require('express-session');
+var cookieParser   = require('cookie-parser');
+var errorHandler   = require('errorhandler');
+var path           = require('path');
+var mongoose       = require('mongoose');
+var passport       = require('passport');
+var LocalStrategy  = require('passport-local').Strategy;
+
+// Load my database configurations
+var configDB       = require('./config/database.js');
+
+// Require user model for setting up test user.
+var User = require('./models/user');
 
 var getDbConnection = function() {
-	// set up the db connection
-
-	var configDB = require('./config/database.js');
 
 	mongoose.connect(configDB.url);
 
@@ -19,80 +28,94 @@ var getDbConnection = function() {
 		console.log('Database connection established.');
 		runExpressServer();
 	});
-}
+};
 
 var runExpressServer = function() {
-	// create an instansce of express
+	// create an express instance
 	var app = express();
+	var env = process.env.NODE_ENV || 'development';
 
-	// Configuration for the express instance
-	app.configure(function() {
+	// set up express application
+	app.set('port', process.env.PORT || 8080);
+	app.use(morgan('dev'));		// 'default', 'short', 'tiny', 'dev'
+	app.use(bodyParser());
+	app.use(methodOverride());
 
-		// set up express application
-		app.set('port', process.env.PORT || 8080);
-		app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
-		app.use(express.bodyParser());
-		app.use(express.methodOverride());
+	// Required for passport
+	// Cookie parser has to be before session. Is required for sessions.
+	app.use(cookieParser('A0K!nr6Wo)ZZ4.Xp!ddAOp8&tw!vn4Erpqnvr4982!'));	// Use signed cookies
+	app.use(session({
+		cookie : {
+			maxAge: 3600000 * 24 // 1h in ms * 24
+		},
+		secret: 'A0K!nr6Wo)ZZ4.Xp!ddAOp8&tw!vn4Erpqnvr4982!'
+	}));
 
-		// required for passport
-		// cookie parser has to be before session
-		// is required for sessions
-		app.use(express.cookieParser('A0K!nr6Wo)ZZ4.Xp!ddAOp8&tw!vn4Erpqnvr4982!')); // use signed cookies
-		app.use(express.session({
-			cookie : {
-				maxAge: 3600000 * 24 // 1h * 24h
-			},
-			secret: 'A0K!nr6Wo)ZZ4.Xp!ddAOp8&tw!vn4Erpqnvr4982!'
-		}));
-		app.use(passport.initialize());
-		app.use(passport.session()); // persistent login sessions
-		app.use(app.router);
+	app.use(passport.initialize());
+	app.use(passport.session());	// Use persistent login sessions
 
-		app.use(express.static(path.join(__dirname, 'public')));
-	});
+	app.use(express.static(path.join(__dirname, 'public')));
 
-	// init error handling
-	app.configure('development', function(){
-    	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-	});
+	// Init error handling regarding environment mode
+	if ('development' === env) {
+		app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+	}
+	else if('production' === env) {
+		app.use(errorHandler());
+	}
 
-	app.configure('production', function(){
-    	app.use(express.errorHandler());
-	});
-
-	// passport config
-	var User = require('./models/user');
-	// create a test user for log in.
-	// first remove the exisitng user and create a new one
+	// NOTE: For testing purpose only!
+	console.log('Setting up test user');
+	// Create a test user for log in. First remove the existisng user and create a new one
 	User.remove({username: 'test'}, function (err) {
 		if (err) {
-			console.log('Error while removing');
+			console.log('Error while removing test user.');
 			return handleError(err);
 		}
+		registerUser(); // register test user again
 	});
-	var hashedClientPW = '1e2e9fc2002b002d75198b7503210c05a1baac4560916a3c6d93bcce3a50d7f00fd395bf1647b9abb8d1afcc9c76c289b0c9383ba386a956da4b38934417789e';
- 	User.register(new User({ username : 'test'}), hashedClientPW, function(err, account) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-        	console.log('Test-user registered.');
-        }
-    });
 
- 	// Load the config for passport
+	// Helper function to register a new test user after existing user has been removed
+	function registerUser() {
+		var hashedClientPW = '1e2e9fc2002b002d75198b7503210c05a1baac4560916a3c6d93bcce3a50d7f00fd395bf1647b9abb8d1afcc9c76c289b0c9383ba386a956da4b38934417789e';
+		User.register(new User({ username : 'test'}), hashedClientPW, function(err, account) {
+			if (err) {
+				console.log(err);
+			}
+			else {
+				console.log('Test-user registered.');
+			}
+		});
+	}
+
+	// Load the config for passport
 	require('./config/passport')(passport);
 
-	// routes
+	// Simple logger for requests
+	app.use(function(req, res, next) {
+		if ('development' === env) {
+			console.log('# ---- Start of Request ---- #');
+		}
+
+		console.log('%s %s', req.method, req.url);
+		next();
+
+		if('development' === env) {
+			console.log('# ----- End of Request ----- #');
+		}
+	});
+
+
+    // Load the routes
 	require('./routes')(app, passport);
 
 	// launch application
 	app.listen(app.get('port'), function() {
 		console.log(("Express server listening on port " + app.get('port')));
 	});
-}
+};
 
 
 exports.start = function(database) {
 	getDbConnection();
-}
+};
